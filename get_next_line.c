610e982f84a31a0bcc7bcfc12fadf724cfd28ac4
Shane CONSTABLE <sconstab@c4r18s2.wethinkcode.co.za>
2019-07-03 16:45:19 +0200

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,63 @@
 /*   By: sconstab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:30:06 by sconstab          #+#    #+#             */
-/*   Updated: 2019/06/21 06:27:40 by sconstab         ###   ########.fr       */
+/*   Updated: 2019/07/03 16:41:42 by sconstab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_readfile(const int fd)
+{
+	char	*got;
+	char	*buff;
+
+	buff = ft_strnew(BUFF_SIZE);
+	got = ft_strnew(0);
+	while (read(fd, buff, BUFF_SIZE) > 0)
+	{
+		got = ft_strjoin(got, buff);
+		buff = ft_strnew(BUFF_SIZE);
+	}
+	return (got);
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	size_t			r;
-	static	size_t	i;
-	void			*got;
+	static size_t	i;
+	static void		*got;
 
-	if (!i)
-		i = 0;
-	if (!(got = ft_memalloc(BUFF_SIZE)))
+	if (fd < 0 || !line || BUFF_SIZE < 0 || read(fd, NULL, 0) == -1)
 		return (-1);
-	r = read(fd, got, BUFF_SIZE);
-	if (!(line[i] = ft_memalloc(ft_strcnlen(got, '\n', i) * sizeof(void))))
-		return (-1);
-	line[i] = ft_strcreturn(got, '\n', i);
-	i++;
-	return (r);
+	if (got == NULL)
+		got = ft_readfile(fd);
+	while (i < (ft_wordcount(got, '\n')))
+	{
+		*line = ft_strcreturn(got, '\n', i);
+		i++;
+		return (1);
+	}
+	//free(line);
+	return (0);
 }
+/*
+int	main(int argc, char **argv)
+{
+	int fd;
+	int i;
+	char *buff;
+
+	i = 1;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while  (i > 0)
+		{
+			i = get_next_line(fd, &buff);
+			if (i != 0)
+				ft_putnbr(ft_strcmp(buff, "111"));
+			free(buff);
+		}
+	}
+	return (0);
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sconstab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 07:59:04 by sconstab          #+#    #+#             */
-/*   Updated: 2019/06/20 08:58:14 by sconstab         ###   ########.fr       */
+/*   Updated: 2019/07/03 14:05:13 by sconstab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 # define BUFF_SIZE 32
 
 # include "libft/libft.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 int		get_next_line(const int fd, char **line);
 
