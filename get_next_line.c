27fb2c21f23a5319e1a436b9438534d658aa8645
Shane CONSTABLE <sconstab@c4r18s2.wethinkcode.co.za>
2019-07-04 16:12:41 +0200

    Almost Done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sconstab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:30:06 by sconstab          #+#    #+#             */
-/*   Updated: 2019/07/03 16:41:42 by sconstab         ###   ########.fr       */
+/*   Updated: 2019/07/04 16:06:56 by sconstab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@ static char	*ft_readfile(const int fd)
 	char	*got;
 	char	*buff;
 
-	buff = ft_strnew(BUFF_SIZE);
 	got = ft_strnew(0);
+	buff = ft_strnew(BUFF_SIZE);
 	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
 		got = ft_strjoin(got, buff);
 		buff = ft_strnew(BUFF_SIZE);
 	}
+	free(buff);
 	return (got);
 }
 
-int	get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	static size_t	i;
 	static void		*got;
@@ -42,27 +43,6 @@ int	get_next_line(const int fd, char **line)
 		i++;
 		return (1);
 	}
-	//free(line);
+	free(got);
 	return (0);
 }
-/*
-int	main(int argc, char **argv)
-{
-	int fd;
-	int i;
-	char *buff;
-
-	i = 1;
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		while  (i > 0)
-		{
-			i = get_next_line(fd, &buff);
-			if (i != 0)
-				ft_putnbr(ft_strcmp(buff, "111"));
-			free(buff);
-		}
-	}
-	return (0);
-}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,17 @@
 /*   By: sconstab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 07:59:04 by sconstab          #+#    #+#             */
-/*   Updated: 2019/07/03 14:05:13 by sconstab         ###   ########.fr       */
+/*   Updated: 2019/07/04 16:06:07 by sconstab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE	32
 
 # include "libft/libft.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/uio.h>
-#include <unistd.h>
 
-int		get_next_line(const int fd, char **line);
+int	get_next_line(const int fd, char **line);
 
 #endif
